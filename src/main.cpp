62542f481d25d8452b30c28f89bc4ba397a8e46a
SkyDriver2500 <joaosala3@gmail.com>
2016-09-25 21:21:20 +0000

    print usage

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,28 @@ void printUsage(char **args) {
 }
 
 int main(int argc, char **argv) {
+
+	if(argc != 3) {
+		printUsage(argv);
+		exit(EXIT_FAILURE);
+	}
+	
 	Bitmap bitmap;
 	SourceFile sfile(argv[1]);
 	char* path;
 	path = (char*) malloc (1024);
-	
+
 	strcpy(path, getenv("HOME"));
 	strcat(path, "/Pictures/");
 	strcat(path, argv[2]);
-	
+
 	if(sfile.readfile()) {
 		cout << "[ + ] File read successfully." << endl;
 	} else {
 		cout << "[ - ] Couldn't read file." << endl;
 		exit(2);
 	}
-	
+
 	if(bitmap.createbmp(path, sfile.getcontent())) {
 		cout << "[ + ] Bitmap created. Saving under "
 			<< path << endl;
